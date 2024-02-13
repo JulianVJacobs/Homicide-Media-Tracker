f4542b7edceb8ac68c14e3343435b4a7662e1206
MaxB543 <maxibrenner@gmail.com>
2024-02-13 17:11:10 +0200

    added comments and readmes

---
+++ README.txt
@@ -0,0 +1,5 @@
+WELCOME TO THE HOMICIDE MEDIA TRACKER. 
+To initialise the app, please open the terminal and type in command: npm start 
+This will start both the server and the client app.
+The App should open soon after the command is executed. 
+If the App does not open, go to your browser/safari and type in the url: http://localhost:3000
--- server/db.js
+++ server/db.js
@@ -1,3 +1,7 @@
+//this is the database info file. it contains useful information such as the port number --set default at 5432
+//the database name, password and user. it is important to ensure that your database details are identical to these, or if they
+//are different, to change this file to match them. 
+
 const Pool = require("pg").Pool;
 
 const pool = new Pool({

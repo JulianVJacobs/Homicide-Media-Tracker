5a513d709bb55fd71d8ed8ac1d7ef6d53e80cc8c
MaxB543 <maxibrenner@gmail.com>
2024-02-18 11:29:49 +0200

    fixed database issues

--- server/db.js
+++ server/db.js
@@ -6,10 +6,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Wotalotigot1!",//"postgres" is the default password, IF YOU ALREADY HAVE POSTGRESQL SET UP ON YOUR MACHINE, REPLACE WITH YOUR ACTUAL PASSWORD
+  password: "postgres",//"postgres" is the default password, IF YOU ALREADY HAVE POSTGRESQL SET UP ON YOUR MACHINE, REPLACE WITH YOUR ACTUAL PASSWORD
   host: "localhost",
   port: 5432,
-  database: "Homicide_main",
+  database: "homicide_main",
   ssl: false, 
 });
 
